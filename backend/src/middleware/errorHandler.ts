28f85d8c4db0a1ca8c2ef4ac921101e802554afe
Vinícius Luiz Santa Rosa <107439773+vini2luiz@users.noreply.github.com>
2025-09-16 19:10:25 -0300

    Delete backend directory
    backend com defeitos

--- backend/src/middleware/errorHandler.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-
-export function errorHandler(err: any, req: Request, res: Response, next: NextFunction) {
-  if (err && err.status && err.message) {
-    res.status(err.status).json({ error: err.message });
-  } else {
-    res.status(500).json({ error: 'Erro interno do servidor' });
-  }
-}
